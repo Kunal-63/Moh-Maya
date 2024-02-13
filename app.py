f5f7f136625ed1bf6b2af19169e267e13566b436
Kunal-63 <kunaladwani1456@gmail.com>
2024-02-13 23:01:01 +0530

    vercel testing

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
