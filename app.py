9180fc6018718ec7d10545e94dcc2829f65079ce
Kunal-63 <kunaladwani1456@gmail.com>
2024-02-15 22:33:43 +0530

    adding data to landing page

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('landing_page.html')
 
